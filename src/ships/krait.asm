a7c5701ffba8d135eb81551f65b089fb7bba75dc
eelcohn <eelco@huininga.nl>
2019-08-18 13:29:37 +0200

    Update krait.asm

--- src/ships/krait.asm
+++ src/ships/krait.asm
@@ -12,13 +12,13 @@
 	EQUW &0E10                      ; Area for missile lock
 	EQUB &7A                        ; Edges data info offset lo
 	EQUB &CE                        ; Faces data info offset lo
-	EQUB &55                        ; Ship lines stack (4*maxlines+1)
-	EQUB &00                        ; Gun vertex*4
-	EQUB &12                        ; Explosion count (4*n+6)
-	EQUB &66                        ; 17 Vertices (17*6)
-	EQUB &15                        ; 
+	EQUB &55                        ; Ship lines stack = 21 (4*maxlines+1)
+	EQUB &00                        ; Gun vertex = 0 (vertex*4)
+	EQUB &12                        ; Explosion count = 3 (4*n+6)
+	EQUB &66                        ; 17 Vertices (n*6)
+	EQUB &15                        ; 21 Edges
 	EQUW &0064                      ; Bounty
-	EQUB &18                        ; Faces*4
+	EQUB &18                        ; 6 Faces (n*4)
 	EQUB &14                        ; Dot beyond distance
 	EQUB &50                        ; Energy
 	EQUB &1E                        ; Speed
@@ -30,6 +30,7 @@
 ; -----------------------------------------------------------------------------
 ; Vertices data
 ; -----------------------------------------------------------------------------
+.ship_krait_vertices
 	EQUB &00, &00, &60, &1F, &01, &23
 	EQUB &00, &12, &30, &3F, &03, &45
 	EQUB &00, &12, &30, &7F, &12, &45
@@ -49,8 +50,9 @@
 	EQUB &24, &00, &1E, &A8, &55, &55
 
 ; -----------------------------------------------------------------------------
-; Edges and face data
+; Edges data
 ; -----------------------------------------------------------------------------
+.ship_krait_edges
 	EQUB &1F, &03, &00, &04, &1F, &12, &00, &08
 	EQUB &1F, &01, &00, &0C, &1F, &23, &00, &10
 	EQUB &1F, &35, &04, &10, &1F, &25, &10, &08
@@ -61,9 +63,14 @@
 	EQUB &06, &33, &20, &24, &08, &44, &2C, &34
 	EQUB &08, &44, &34, &30, &07, &44, &30, &2C
 	EQUB &07, &55, &38, &3C, &08, &55, &3C, &40
-	EQUB &08, &55, &40, &38, &1F, &07, &30, &06
-	EQUB &5F, &07, &30, &06, &DF, &07, &30, &06
-	EQUB &9F, &07, &30, &06, &3F, &4D, &00, &9A
-	EQUB &BF, &4D, &00, &9A
+	EQUB &08, &55, &40, &38
+
+; -----------------------------------------------------------------------------
+; Faces data
+; -----------------------------------------------------------------------------
+.ship_krait_faces
+	EQUB &1F, &07, &30, &06, &5F, &07, &30, &06
+	EQUB &DF, &07, &30, &06, &9F, &07, &30, &06
+	EQUB &3F, &4D, &00, &9A, &BF, &4D, &00, &9A
 
 .ship_krait_end
