4d07a58569d59a0425e4ef1257fb5202b1a69151
eelcohn <eelco@huininga.nl>
2019-08-29 00:10:54 +0200

    Update D.MOA.asm

--- src/D.MOA.asm
+++ src/D.MOA.asm
@@ -7,122 +7,118 @@
 ; Length         : 00000A00
 ; -----------------------------------------------------------------------------
 
-ship_none    = &0000
-ship_missile = &7F00
+none_start     = &0000
+none_attr      = %00000000              ; 7.......: Escape pod present
+                                        ; .6......: Galcop police ship
+                                        ; ..5.....: Ship is protected by spacestation
+                                        ; ...4....: Ship is docking
+                                        ; ....3...: Ship is a pirate
+                                        ; .....2..: Ship is attacking you
+                                        ; ......1.: Ship is a bounty hunter
+                                        ; .......0: Ship is a trader
 
 ORG &5600
 
 .start
 
 .ship_pointers
-	EQUW ship_missile               ; Ship type 01: Missile
-	EQUW ship_coriolis              ; Ship type 02: Space station
-	EQUW ship_escape_pod            ; Ship type 03: Esacape pod
-	EQUW ship_alloy                 ; Ship type 04: Plate/Alloy
-	EQUW ship_barrel                ; Ship type 05: Barrel
-	EQUW ship_none                  ; Ship type 06: Boulder
-	EQUW ship_none                  ; Ship type 07: Asteroid
-	EQUW ship_none                  ; Ship type 08: Splinter
-	EQUW ship_none                  ; Ship type 09: Shuttle
-	EQUW ship_none                  ; Ship type 10: Transporter
-	EQUW ship_none                  ; Ship type 11: Cobra MkIII
-	EQUW ship_python                ; Ship type 12: Python
-	EQUW ship_none                  ; Ship type 13: Boa
-	EQUW ship_none                  ; Ship type 14: Anaconda
-	EQUW ship_none                  ; Ship type 15: 
-	EQUW ship_viper                 ; Ship type 16: Viper
-	EQUW ship_sidewinder            ; Ship type 17: Sidewinder
-	EQUW ship_mamba                 ; Ship type 18: Mamba
-	EQUW ship_krait                 ; Ship type 19: Krait
-	EQUW ship_none                  ; Ship type 20: Adder
-	EQUW ship_none                  ; Ship type 21: Gecko
-	EQUW ship_none                  ; Ship type 22: Cobra MkI
-	EQUW ship_none                  ; Ship type 23: Worm
-	EQUW ship_cobra_mk3_pirate      ; Ship type 24: Cobra MkIII (pirate)
-	EQUW ship_none                  ; Ship type 25: Asp MkII
-	EQUW ship_none                  ; Ship type 26: Python (pirate)
-	EQUW ship_fer_de_lance          ; Ship type 27: Fer de Lance
-	EQUW ship_none                  ; Ship type 28: Moray
-	EQUW ship_none                  ; Ship type 29: Thargoid
-	EQUW ship_none                  ; Ship type 30: Thargon
-	EQUW ship_none                  ; Ship type 31: Constrictor
+	EQUW missile_start              ; Ship type 01: Missile
+	EQUW spacestation_start         ; Ship type 02: Space station
+	EQUW escape_pod_start           ; Ship type 03: Escape pod
+	EQUW alloy_start                ; Ship type 04: Plate/Alloy
+	EQUW barrel_start               ; Ship type 05: Barrel
+	EQUW none_start                 ; Ship type 06: Boulder
+	EQUW none_start                 ; Ship type 07: Asteroid
+	EQUW none_start                 ; Ship type 08: Splinter
+	EQUW none_start                 ; Ship type 09: Shuttle
+	EQUW none_start                 ; Ship type 0A: Transporter
+	EQUW none_start                 ; Ship type 0B: Cobra MkIII
+	EQUW python_start               ; Ship type 0C: Python
+	EQUW none_start                 ; Ship type 0D: Boa
+	EQUW none_start                 ; Ship type 0E: Anaconda
+	EQUW none_start                 ; Ship type 0F: Ship &0F unknown
+	EQUW viper_start                ; Ship type 10: Viper
+	EQUW sidewinder_start           ; Ship type 11: Sidewinder
+	EQUW mamba_start                ; Ship type 12: Mamba
+	EQUW krait_start                ; Ship type 13: Krait
+	EQUW none_start                 ; Ship type 14: Adder
+	EQUW none_start                 ; Ship type 15: Gecko
+	EQUW none_start                 ; Ship type 16: Cobra MkI
+	EQUW none_start                 ; Ship type 17: Worm
+	EQUW cobra_mk3_pirate_start     ; Ship type 18: Cobra MkIII (pirate)
+	EQUW none_start                 ; Ship type 19: Asp Mk2
+	EQUW none_start                 ; Ship type 1A: Python (pirate)
+	EQUW fer-de-lance_start         ; Ship type 1B: Fer de Lance
+	EQUW none_start                 ; Ship type 1C: Moray
+	EQUW none_start                 ; Ship type 1D: Thargoid
+	EQUW none_start                 ; Ship type 1E: Thargon
+	EQUW none_start                 ; Ship type 1F: Constrictor
 
-.ship_attributes                    ; Ship attributes:
-                                        ; x....... bit 7: Escape pod present
-                                        ; .x...... bit 6: Galcop police ship
-                                        ; ..x.....: Ship is protected by spacestation
-                                        ; ...x....: Ship is docking
-                                        ; ....x...: Ship is a pirate
-                                        ; .....x..: Ship is attacking you
-                                        ; ......x.: Ship is a bounty hunter
-                                        ; .......x: Ship is a trader
-	EQUB %????????                  ; Ship type 01: Missile
-	EQUB %00000000                  ; Ship type 02: Space station
-	EQUB %00000001                  ; Ship type 03: Escape pod
-	EQUB %00000000                  ; Ship type 04: Plate/alloy
-	EQUB %00000000                  ; Ship type 05: Barrel
-	EQUB %00000000                  ; Ship type 06: Boulder
-	EQUB %00000000                  ; Ship type 07: Asteroid
-	EQUB %00000000                  ; Ship type 08: Splinter
-	EQUB %00000000                  ; Ship type 09: Shuttle
-	EQUB %00000000                  ; Ship type 10: Transporter
-	EQUB %00000000                  ; Ship type 11: Cobra MkIII
-	EQUB %10100000                  ; Ship type 12: Python
-	EQUB %00000000                  ; Ship type 13: Boa
-	EQUB %00000000                  ; Ship type 14: Anaconda
-	EQUB %00000000                  ; Ship type 15: 
-	EQUB %11000010                  ; Ship type 16: Viper
-	EQUB %00001100                  ; Ship type 17: Sidewinder
-	EQUB %10001100                  ; Ship type 18: Mamba
-	EQUB %10001100                  ; Ship type 19: Krait
-	EQUB %00000000                  ; Ship type 20: Adder
-	EQUB %00000000                  ; Ship type 21: Gecko
-	EQUB %00000000                  ; Ship type 22: Cobra MkI
-	EQUB %00000000                  ; Ship type 23: Worm
-	EQUB %10001100                  ; Ship type 24: Cobra MkIII (pirate)
-	EQUB %00000000                  ; Ship type 25: Asp MkII
-	EQUB %00000000                  ; Ship type 26: Python (pirate)
-	EQUB %10000010                  ; Ship type 27: Fer de Lance
-	EQUB %00000000                  ; Ship type 28: Moray
-	EQUB %00000000                  ; Ship type 29: Thargoid
-	EQUB %00000000                  ; Ship type 30: Thargon
-	EQUB %00000000                  ; Ship type 31: Constrictor
+.ship_attr                               ; Ship attributes:
+	EQUB missile_attr               ; Ship type 1: Missile
+	EQUB spacestation_attr          ; Ship type 2: Space station
+	EQUB escape_pod_attr            ; Ship type 3: Escape pod
+	EQUB alloy_attr                 ; Ship type 4: Plate/Alloy
+	EQUB barrel_attr                ; Ship type 5: Barrel
+	EQUB none_attr                  ; Ship type 6: Boulder
+	EQUB none_attr                  ; Ship type 7: Asteroid
+	EQUB none_attr                  ; Ship type 8: Splinter
+	EQUB none_attr                  ; Ship type 9: Shuttle
+	EQUB none_attr                  ; Ship type 10: Transporter
+	EQUB none_attr                  ; Ship type 11: Cobra MkIII
+	EQUB python_attr                ; Ship type 12: Python
+	EQUB none_attr                  ; Ship type 13: Boa
+	EQUB none_attr                  ; Ship type 14: Anaconda
+	EQUB none_attr                  ; Ship type 15: Ship &0F unknown
+	EQUB viper_attr                 ; Ship type 16: Viper
+	EQUB sidewinder_attr            ; Ship type 17: Sidewinder
+	EQUB mamba_attr                 ; Ship type 18: Mamba
+	EQUB krait_attr                 ; Ship type 19: Krait
+	EQUB none_attr                  ; Ship type 20: Adder
+	EQUB none_attr                  ; Ship type 21: Gecko
+	EQUB none_attr                  ; Ship type 22: Cobra MkI
+	EQUB none_attr                  ; Ship type 23: Worm
+	EQUB cobra_mk3_pirate_attr      ; Ship type 24: Cobra MkIII (pirate)
+	EQUB none_attr                  ; Ship type 25: Asp Mk2
+	EQUB none_attr                  ; Ship type 26: Python (pirate)
+	EQUB fer-de-lance_attr          ; Ship type 27: Fer de Lance
+	EQUB none_attr                  ; Ship type 28: Moray
+	EQUB none_attr                  ; Ship type 29: Thargoid
+	EQUB none_attr                  ; Ship type 30: Thargon
+	EQUB none_attr                  ; Ship type 31: Constrictor
 
-.ship_coriolis
-INCLUDE "src/ships/coriolis.asm"
+.ship_spacestation
+	INCLUDE "src/ships/spacestation.asm"
 
 .ship_escape_pod
-INCLUDE "src/ships/escape_pod.asm"
+	INCLUDE "src/ships/escape_pod.asm"
 
 .ship_alloy
-INCLUDE "src/ships/alloy.asm"
+	INCLUDE "src/ships/alloy.asm"
 
 .ship_barrel
-INCLUDE "src/ships/barrel.asm"
+	INCLUDE "src/ships/barrel.asm"
 
 .ship_python
-INCLUDE "src/ships/python.asm"
+	INCLUDE "src/ships/python.asm"
 
 .ship_viper
-INCLUDE "src/ships/viper.asm"
+	INCLUDE "src/ships/viper.asm"
 
 .ship_sidewinder
-INCLUDE "src/ships/sidewinder.asm"
+	INCLUDE "src/ships/sidewinder.asm"
 
 .ship_mamba
-INCLUDE "src/ships/mamba.asm"
+	INCLUDE "src/ships/mamba.asm"
 
 .ship_krait
-INCLUDE "src/ships/krait.asm"
+	INCLUDE "src/ships/krait.asm"
 
 .ship_cobra_mk3_pirate
-INCLUDE "src/ships/cobra_mk3.asm"
-ORG ship_cobra_mk3_pirate
-	EQUB &01
-ORG ship_cobra_mk3_pirate+&0A
-	EQUB &AF
-ORG ship_cobra_mk3_pirate+&13
-	EQUB &12
+	INCLUDE "src/ships/cobra_mk3_pirate.asm"
+
+.ship_fer-de-lance
+	INCLUDE "src/ships/fer-de-lance.asm"
 
 	FOR n, *, (&6000 - P%)
 		EQUB &00
